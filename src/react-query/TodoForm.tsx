545d4a98123c703e611eadc61ac50340c07266aa
omurilolima <msl@cesar.school>
2025-06-19 18:34:55 +0100

    Mutating data / Submitting form

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,18 +1,60 @@
-import { useRef } from 'react';
+import {
+	QueryClient,
+	useMutation,
+	useQueryClient,
+} from "@tanstack/react-query";
+import { useRef } from "react";
+import { Todo } from "./hooks/useTodos";
+import axios from "axios";
 
 const TodoForm = () => {
-  const ref = useRef<HTMLInputElement>(null);
+	const queryClient = useQueryClient();
 
-  return (
-    <form className="row mb-3">
-      <div className="col">
-        <input ref={ref} type="text" className="form-control" />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary">Add</button>
-      </div>
-    </form>
-  );
+	const addTodo = useMutation({
+		mutationFn: (todo: Todo) =>
+			axios
+				.post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
+				.then((res) => res.data),
+		// callback function to access the response from the server and update the page
+		onSuccess: (savedTodo, newTodo) => {
+			// APPROACH 1: Invalidating the cache
+			// queryClient.invalidateQueries({
+			// 	queryKey: ["todos"],
+			// });
+
+			// APPROACH 2: Updating the data in the cache directly
+			queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+				savedTodo,
+				...(todos || []),
+			]);
+		},
+	});
+
+	const ref = useRef<HTMLInputElement>(null);
+
+	return (
+		<form
+			className="row mb-3"
+			onSubmit={(event) => {
+				event.preventDefault();
+
+				if (ref.current && ref.current.value)
+					addTodo.mutate({
+						id: 0,
+						title: ref.current?.value,
+						completed: false,
+						userId: 1,
+					});
+			}}
+		>
+			<div className="col">
+				<input ref={ref} type="text" className="form-control" />
+			</div>
+			<div className="col">
+				<button className="btn btn-primary">Add</button>
+			</div>
+		</form>
+	);
 };
 
 export default TodoForm;
